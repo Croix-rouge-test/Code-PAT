a438c21c7b0e30a55b1281c250b39509986f95f0
root <clement.dilasser@croix-rouge.fr>
2026-02-07 11:22:50 +0000

    Modifications pour intégration au main

--- scripts_data/alim.py
+++ scripts_data/alim.py
@@ -54,47 +54,46 @@ def clean_alim(df_alim):
     # Identifier les doublons dans la colonne 'Code U2A'
     duplicates = df_alim[df_alim['Code U2A'].duplicated(keep=False)]
 
-    # Compter le nombre de doublons
-    num_duplicates = duplicates.shape[0]
+  #   # Compter le nombre de doublons
+  #   num_duplicates = duplicates.shape[0]
 
-    if num_duplicates > 0:
-          print(f"Il y a {num_duplicates} lignes avec des doublons dans la colonne 'Code U2A'.")
-          print("Voici les lignes en doublon (affichant toutes les occurrences des valeurs dupliquées) :")
-          display(duplicates.sort_values(by='Code U2A'))
-    else:
-          print("Aucun doublon trouvé dans la colonne 'Code U2A'.")
-  else:
-        print("La colonne 'Code U2A' n'existe pas dans le DataFrame df_alim.")
+  #   if num_duplicates > 0:
+  #         print(f"Il y a {num_duplicates} lignes avec des doublons dans la colonne 'Code U2A'.")
+  #         print("Voici les lignes en doublon (affichant toutes les occurrences des valeurs dupliquées) :")
+  #         display(duplicates.sort_values(by='Code U2A'))
+  #   else:
+  #         print("Aucun doublon trouvé dans la colonne 'Code U2A'.")
+  # else:
+  #       print("La colonne 'Code U2A' n'existe pas dans le DataFrame df_alim.")
  
   # Création d'une base de données sans doublons
   df_alim_sans_doublons =df_alim.drop_duplicates(subset=['Code U2A'], keep='first')
-  print(f"Taille du DataFrame après suppression des doublons : {df_alim_sans_doublons.shape}")
+  # print(f"Taille du DataFrame après suppression des doublons : {df_alim_sans_doublons.shape}")
   df_alim_sans_doublons = df_alim_sans_doublons.rename(columns={"N° structure": "n_structure"})
   # Conversion de la colonne n_structure en string pour la cohérence avec df_ref_structure
   df_alim_sans_doublons['n_structure'] = df_alim_sans_doublons['n_structure'].astype(str)
   # Supprimer le '.0' des chaînes si elles proviennent de nombres flottants
   df_alim_sans_doublons['n_structure'] = df_alim_sans_doublons['n_structure'].apply(lambda x: x[:-2] if x.endswith('.0') else x)
-  display(df_alim_sans_doublons.head())
 
+  return df_alim_sans_doublons
+
+def indicateurs_alim(df_alim_sans_doublons, df_ref_structure):
     # U2A
-  if 'Dispositif' in df_alim_sans_doublons.columns:
-      print("Nombre d'occurrences pour chaque type de 'Dispositif':")
-      display(df_alim_sans_doublons['Dispositif'].value_counts())
-  else:
-      print("La colonne 'Dispositif' n'existe pas dans le DataFrame df_alim_sans_doublons.")
+  # if 'Dispositif' in df_alim_sans_doublons.columns:
+  #     print("Nombre d'occurrences pour chaque type de 'Dispositif':")
+  #     display(df_alim_sans_doublons['Dispositif'].value_counts())
+  # else:
+  #     print("La colonne 'Dispositif' n'existe pas dans le DataFrame df_alim_sans_doublons.")
   
   df_alim_U2A_sans_doublons = df_alim_sans_doublons.groupby("n_structure").size().reset_index(name="Aide_alimentaire Nb_U2A")
-  verifier_colonne_structure(df_alim_U2A_sans_doublons,"n_structure", df_ref_structure)
 
   df_alim_U2A_DT = dt_rattachement(df_alim_U2A_sans_doublons, df_ref_structure)
   df_alim_U2A_DT = df_alim_U2A_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_U2A DT")
 
   # Epicerie sociale
   df_alim_epicerie_sociale= df_alim_sans_doublons[df_alim_sans_doublons['Dispositif'] == 'Epicerie sociale']
-  display(df_alim_epicerie_sociale.head())
   df_alim_epicerie_sociale.shape[0]
   df_alim_epicerie_sociale = df_alim_epicerie_sociale.groupby("n_structure").size().reset_index(name="Aide_alimentaire Nb_epiceries_sociales")
-  verifier_colonne_structure(df_alim_epicerie_sociale,"n_structure", df_ref_structure)
   
   df_alim_epicerie_sociale_DT = dt_rattachement(df_alim_epicerie_sociale, df_ref_structure)
   df_alim_epicerie_sociale_DT = df_alim_epicerie_sociale_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_epiceries_sociales DT")
@@ -102,23 +101,28 @@ def clean_alim(df_alim):
 
   # Accueil Alimentaire
   df_alim_accueil_alimentaire= df_alim_sans_doublons[df_alim_sans_doublons['Dispositif'] .isin(['Accueil alimentaire', 'Accueil Alimentaire'])]
-  display (df_alim_accueil_alimentaire.head())
   df_alim_accueil_alimentaire.shape[0]
   df_alim_accueil_alimentaire = df_alim_accueil_alimentaire.groupby("n_structure").size().reset_index(name="Aide_alimentaire Nb_Centre_distribution_alimentaire")
-  verifier_colonne_structure(df_alim_accueil_alimentaire, "n_structure", df_ref_structure)
   
   df_alim_accueil_alimentaire_DT = dt_rattachement(df_alim_accueil_alimentaire, df_ref_structure)
-  df_alim_accueil_alimentaire_DT = df_accueil_alimentaire_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_Centre_distribution_alimentaire DT")
+  df_alim_accueil_alimentaire_DT = df_alim_accueil_alimentaire_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_Centre_distribution_alimentaire DT")
   
   # CRsr
   df_alim_crsr= df_alim_sans_doublons[df_alim_sans_doublons['Dispositif'].isin(['Croix-Rouge sur Roues', 'CRSR Accueil alimentaire'])]
-  display (df_alim_crsr.head())
   df_alim_crsr.shape[0]
   df_alim_crsr = df_alim_crsr.groupby("n_structure").size().reset_index(name="Aide_alimentaire Nb_crsr")
-  verifier_colonne_structure(df_alim_crsr, "n_structure", df_ref_structure)
   
   df_alim_crsr_DT = dt_rattachement(df_alim_crsr, df_ref_structure)
-  df_alim_crsr_DT = df_crsr_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_crsr DT")
+  df_alim_crsr_DT = df_alim_crsr_DT.groupby("DT_de_rattachement").size().reset_index(name="Aide_alimentaire Nb_crsr DT")
 
-  return df_alim_U2A_sans_doublons, df2, df3
+  return (
+    df_alim_U2A_sans_doublons, 
+    df_alim_epicerie_sociale, 
+    df_alim_accueil_alimentaire, 
+    df_alim_crsr,
+    df_alim_U2A_DT,
+    df_alim_epicerie_sociale_DT,
+    df_alim_accueil_alimentaire_DT,
+    df_alim_crsr_DT
+  )
   
