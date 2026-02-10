497b8b80aede071e4a49e6119154fc9d47c303b9
root <clement.dilasser@croix-rouge.fr>
2026-02-10 11:28:07 +0000

    Ajout code Ana gaia.py

--- scripts_data/gaia.py
+++ scripts_data/gaia.py
@@ -34,6 +34,7 @@ import shutil
 import zipfile
 from google.colab import drive, files
 
+
 from google.colab import auth
 from google.auth import default
 import unicodedata
@@ -45,11 +46,11 @@ sys.path.append(os.path.abspath("/Code-PAT"))
 from utils import *
 
 
-
 def indicateurs_gaia(df_gaia):
   # Vérification que la colonne est au format datetime
-  df_gaia['rattachement_benevole_date_fin'] = pd.to_datetime(df_nomination['rattachement_benevole_date_fin'], errors='coerce')
-  df_gaia['rattachement_benevole_date_debut'] = pd.to_datetime(df_nomination['rattachement_benevole_date_debut'], errors='coerce')
+  df_gaia['rattachement_benevole_date_fin'] = pd.to_datetime(df_gaia['rattachement_benevole_date_fin'], errors='coerce')
+  df_gaia['rattachement_benevole_date_debut'] = pd.to_datetime(df_gaia['rattachement_benevole_date_debut'], errors='coerce')
+
 
   # Filtrage : date nulle ou année = 2025
   df_gaia = df_gaia[
@@ -63,13 +64,64 @@ def indicateurs_gaia(df_gaia):
         | (df_gaia['rattachement_benevole_date_debut'].dt.year != 2026)
     )
   ]
-  
+ 
   df_gaia = df_gaia.rename(columns={
-    'rattachement_benevole_structure_id_fk': 'n_structure'
+    'rattachement_benevole_structure_id_fk': 'n_structure',
+    'rattachement_benevole_nivol_id_fk': 'Structure Nb_Benevoles'
   })
  # On compte le nb de volontaires de l'urgence
-  nb_benevoles = (df_gaia.groupby('n_structure')['rattachement_benevole_nivol_id_fk'].nunique())
+  nb_benevoles = (df_gaia.groupby('n_structure')['Structure Nb_Benevoles'].nunique())
   return nb_benevoles
+
+
+def indicateurs_gaia_nvx(df_gaia):
+  # Vérification que la colonne est au format datetime
+  df_gaia['rattachement_benevole_date_fin'] = pd.to_datetime(df_gaia['rattachement_benevole_date_fin'], errors='coerce')
+  df_gaia['rattachement_benevole_date_debut'] = pd.to_datetime(df_gaia['rattachement_benevole_date_debut'], errors='coerce')
+
+
+  # Filtrage : date debut année = 2025
+  df_gaia = df_gaia[
+      df_gaia['rattachement_benevole_date_debut'].dt.year == 2025
+  ]
+ 
+  df_gaia = df_gaia.rename(columns={
+    'rattachement_benevole_structure_id_fk': 'n_structure',
+    'rattachement_benevole_nivol_id_fk': 'Structure Nb_nvx_Benevoles_2025'
+  })
+ # On compte le nb de volontaires de l'urgence
+  nb_nvx_benevoles = (df_gaia.groupby('n_structure')['Structure Nb_nvx_Benevoles_2025'].nunique())
+  return nb_nvx_benevoles
+
+
+def indicateurs_gaia_DT(nb_benevoles, rattachement_court):
+    # Merge données avec rattachement_court
+    df_nb_benevoles = pd.merge(
+    nb_benevoles,
+    rattachement_court,
+    on="n_structure",
+    how="left"
+    )
+
+
+    # Groupby sur DT_de_rattachement
+    nb_benevoles_DT = (df_nb_benevoles.groupby('DT_de_rattachement')['Structure Nb_Benevoles'].sum())
+    return nb_benevoles_DT
+
+
+def indicateurs_gaia_nvx_DT(nb_nvx_benevoles, rattachement_court):
+    # Merge données avec rattachement_court
+    df_nb_nvx_benevoles = pd.merge(
+    nb_nvx_benevoles,
+    rattachement_court,
+    on="n_structure",
+    how="left"
+    )
+
+
+    # Groupby sur DT_de_rattachement
+    nb_nvx_benevoles_DT = (df_nb_nvx_benevoles.groupby('DT_de_rattachement')['Structure Nb_nvx_Benevoles_2025'].sum())
+    return nb_nvx_benevoles_DT
 
 
 
