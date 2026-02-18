601810b987e043f02741e5a9a8dc335da546e9ab
root <clement.dilasser@croix-rouge.fr>
2026-02-18 09:05:44 +0000

    corrections nomination

--- scripts_data/nomination.py
+++ scripts_data/nomination.py
@@ -91,7 +91,7 @@ def fusion_nomination(df_nomination, df_ref_nomination):
 
 def indicateurs_nomination_AEO(df_NOMINATION, annee=2025):
     # Filtre sur les libellés appropriés
-    libelles_AEO = ["RTAAD", "RLAAD"]
+    libelles_AEO = ["RTAAD", "RLAAD", "RLACOR", "RLDOM", "RLACORA", "RLDOMA"]
     referents_AEO = df_NOMINATION[
         df_NOMINATION["nomination_libcourt"].isin(libelles_AEO)
     ]
