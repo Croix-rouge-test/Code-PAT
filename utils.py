a89e39033d323b7f8cf446306dadbe4be9dec6a8
root <clement.dilasser@croix-rouge.fr>
2026-03-06 16:32:12 +0000

    Ajout Activités AAD facultatives

--- utils.py
+++ utils.py
@@ -765,6 +765,7 @@ def traitement_all_data(liste_df_a_fusionner_toutes_structures, liste_df_a_fusio
     "AEO Nb_PA_dispos_mobiles",
     "AEO Structure_domiciliation_fixe",
     "AEO Structure_ecrivain_public_fixe",
+    'Activités AAD facultatives',
     "Aide_alimentaire Nb_U2A",
     "Aide_alimentaire Nb_Centre_distribution_alimentaire",
     "Aide_alimentaire Nb_epiceries_sociales",
